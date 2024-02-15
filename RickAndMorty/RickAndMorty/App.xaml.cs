dee37abfba71a7752f47acdda55163c5e4fc622d
bdavis81 <brian.davis810128@gmail.com>
2024-02-15 10:54:53 -0500

    Initial Template Xamarin Forms

---
+++ RickAndMorty/RickAndMorty/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RickAndMorty.Services;
+using RickAndMorty.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RickAndMorty
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RickAndMorty/RickAndMorty/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RickAndMorty.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RickAndMorty.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
